0242e0ee5f8422303bb582faa020e5408dc7c0b1
marcio.arrosi <marcio.arrosi@procempa.com.br>
2017-04-12 15:02:45 -0300

    ionic 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
-import { Http } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { Http, HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { Storage, IonicStorageModule } from '@ionic/storage';
 //translate
@@ -84,6 +85,8 @@ export function providers(): any {
 @NgModule({
   declarations: declarations(),
   imports: [
+    BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot({ name: '__happyhour', storeName: 'hpdb' }),
     TranslateModule.forRoot({
