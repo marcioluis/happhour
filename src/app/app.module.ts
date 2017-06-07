be7e9a5e7e0e60463896c17a16c2725223721add
marcio.arrosi <marcio.arrosi@procempa.com.br>
2017-06-07 10:22:11 -0300

    adicionar modulo de providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,9 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 //app component
 import { MyApp } from './app.component';
-//plugins
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { GooglePlus } from '@ionic-native/google-plus';
-import { Facebook } from '@ionic-native/facebook';
-import { SQLite } from "@ionic-native/sqlite";
-import { Globalization } from "@ionic-native/globalization";
-//mocks
-import { GooglePlusMock } from '../mocks/googlePlusMock';
-import { FacebookMock } from '../mocks/facebookMock';
 //providers
-import { Api, Auth, PlaceProvider, Settings, UserProvider, Database, HapphourProvider } from "../providers";
-
+import { Database } from "../providers";
+import { ProvidersModule } from "../providers/providers.module";
 /** 
  * The translate loader needs to know where to load i18n files
  * in Ionic's static asset pipeline.
@@ -28,48 +18,6 @@ import { Api, Auth, PlaceProvider, Settings, UserProvider, Database, HapphourPro
 export function factoryTranslateLoader(http: Http) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
-/**
- * The Settings provider takes a set of default settings for your app.
- *
- * You can add new settings options at any time. Once the settings are saved,
- * these values will not overwrite the saved values (this can be done manually if desired).
- */
-export function provideDefaultSettings(storage: Storage) {
-  return new Settings(storage, { isFirstRun: true, searchRadius: 500 });
-}
-
-export function configureGP() {
-  if (window["cordova"]) {
-    return GooglePlus;
-  }
-  return GooglePlusMock;
-}
-
-export function configureFB() {
-  if (window["cordova"]) {
-    return Facebook;
-  }
-  return FacebookMock;
-}
-
-const providers: any[] = [
-  StatusBar,
-  SplashScreen,
-  SQLite,
-  Globalization,
-  GooglePlus,
-  Facebook,
-  Api,
-  Auth,
-  PlaceProvider,
-  UserProvider,
-  Database,
-  HapphourProvider,
-  //settings provider
-  { provide: Settings, useFactory: provideDefaultSettings, deps: [Storage] },
-  // Keep this to enable Ionic's runtime error handling during development
-  { provide: ErrorHandler, useClass: IonicErrorHandler }
-];
 
 @NgModule({
   declarations: [MyApp],
@@ -84,10 +32,10 @@ const providers: any[] = [
         useFactory: factoryTranslateLoader,
         deps: [Http]
       }
-    })
+    }),
+    ProvidersModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp],
-  providers: providers
+  entryComponents: [MyApp]
 })
 export class AppModule { }
---
+++ src/providers/providers.module.ts
@@ -0,0 +1,44 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { IonicErrorHandler } from 'ionic-angular';
+//plugins
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Facebook } from '@ionic-native/facebook';
+import { SQLite } from "@ionic-native/sqlite";
+import { Globalization } from "@ionic-native/globalization";
+//providers
+import { Api, Auth, PlaceProvider, Settings, UserProvider, Database, HapphourProvider } from "../providers";
+
+/**
+ * The Settings provider takes a set of default settings for your app.
+ *
+ * You can add new settings options at any time. Once the settings are saved,
+ * these values will not overwrite the saved values (this can be done manually if desired).
+ */
+export function provideDefaultSettings(storage: Storage) {
+    return new Settings(storage, { isFirstRun: true, searchRadius: 500 });
+}
+
+@NgModule({
+    providers: [
+        StatusBar,
+        SplashScreen,
+        SQLite,
+        Globalization,
+        GooglePlus,
+        Facebook,
+        Api,
+        Auth,
+        PlaceProvider,
+        UserProvider,
+        Database,
+        HapphourProvider,
+        //settings provider
+        { provide: Settings, useFactory: provideDefaultSettings, deps: [Storage] },
+        // Keep this to enable Ionic's runtime error handling during development
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ],
+})
+export class ProvidersModule { }
